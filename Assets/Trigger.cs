b8be13718b8e909812254b6a4a9b3cc1743b2a27
Frustman <youhk40@naver.com>
2024-06-10 04:09:32 +0900

    Feat : Video

--- Assets/Trigger.cs
+++ Assets/Trigger.cs
@@ -32,7 +32,7 @@ public class Trigger : MonoBehaviour
 
     private IEnumerator CoolDown()
     {
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(0.5f);
         isCoolDown = false;
     }
 }
